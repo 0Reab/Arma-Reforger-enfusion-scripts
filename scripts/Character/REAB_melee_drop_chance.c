c3a1151f767843dd20ed0b8fab56c7ef00b30040
Mihajlo <122540000+0Reab@users.noreply.github.com>
2025-05-15 18:51:41 +0200

    Upload first script
    Script that rolls a chance on melee attack to drop the equipped weapon.

---
+++ scripts/Character/REAB_melee_drop_chance.c
@@ -0,0 +1,51 @@
+modded class SCR_CharacterControllerComponent : CharacterControllerComponent
+{
+	bool DEBUG = false; // enables print statements for debugging
+	
+	int GetChance()
+	{
+		RandomGenerator gen = new RandomGenerator();
+		int chance = gen.RandIntInclusive(1, 10);
+		
+		if (DEBUG) PrintFormat("chance -> %1", chance);
+		
+		return chance;
+	}
+	
+	//--------------------------------------------------------------------------------------------------------------------------------------
+	
+	WeaponSlotComponent GetWpnSlot()
+	{
+		BaseWeaponManagerComponent WpnManager = null;
+        ChimeraCharacter character = GetCharacter();
+		
+		WpnManager = BaseWeaponManagerComponent.Cast(character.FindComponent(BaseWeaponManagerComponent));
+		WeaponSlotComponent slot = WpnManager.GetCurrentSlot();
+		
+		if (!character || !WpnManager || !slot)
+		{
+			if (DEBUG) Print("WpnManager || character || slot -> failed to initialize");
+			return null;
+		}
+		
+		return slot;
+	}
+	
+	//--------------------------------------------------------------------------------------------------------------------------------------	
+	
+	override void OnMeleeDamage(bool started)
+	{
+		m_MeleeComponent.SetMeleeAttackStarted(started);
+		int chance = GetChance();
+		
+		if (chance == 1)
+		{
+			WeaponSlotComponent slot = GetWpnSlot();
+			
+            DropWeapon(slot);
+            if (DEBUG) Print("Weapon dropped!!!");
+		}
+
+		//if (GetCanFireWeapon())
+	}
+}
